62ac51f646901224b7387faf2befb6eeb35da325
SHASHI KALWAR <138351592+shashishivgit909@users.noreply.github.com>
2024-10-20 23:07:03 +0530

    added colors for project as per theme

--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -1,5 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
 
 const routers=createBrowserRouter([
+{
+    path:"/",
+    element:<App/>,
+    children:[
+        {
+            path:"",
+            element:<h1>Home</h1>
 
+        },
+        {
+            path:"/orders",
+            element:<h1>Orders</h1>
+
+        },
+        {
+            path:"/about",
+            element:<h1>About</h1>
+
+        }
+    ]
+}
 ]);
+
+export default routers;
