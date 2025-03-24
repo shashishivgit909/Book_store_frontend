0af3077cadda9bd074a06c2c1353267df0e67715
SHASHI KALWAR <138351592+shashishivgit909@users.noreply.github.com>
2025-03-24 21:33:27 +0530

    changes

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,13 +5,22 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_APIKEY,
+//   authDomain: import.meta.env.VITE_AUTHDOMAIN,
+//   projectId: import.meta.env.VITE_PROJECTID,
+//   storageBucket: import.meta.env.VITE_STORGAEBUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+//   appId: import.meta.env.VITE_APPID
+// };
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORGAEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID
+  apiKey: "AIzaSyATbGsBU930jHF96E-1q8yHeqZ2euBTaBE",
+  authDomain: "bookstore-mern-app-31d2c.firebaseapp.com",
+  projectId: "bookstore-mern-app-31d2c",
+  storageBucket: "bookstore-mern-app-31d2c.firebasestorage.app",
+  messagingSenderId: "942743173411",
+  appId: "1:942743173411:web:52e950b13e707d25883496"
 };
 
 // Initialize Firebase
